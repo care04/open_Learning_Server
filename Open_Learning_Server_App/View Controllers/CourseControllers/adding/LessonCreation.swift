25e9497fd779fb32dd281bb49127ef8895c72b77
care04 <carefarrar@gmail.com>
2022-02-14 10:52:03 -0500

    hooked up the text view and field and did some customization to the text view

--- Open_Learning_Server_App/View Controllers/CourseControllers/adding/LessonCreation.swift
+++ Open_Learning_Server_App/View Controllers/CourseControllers/adding/LessonCreation.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class LessonCreation: UIViewController {
+class LessonCreation: UIViewController, UITextViewDelegate {
 
+  @IBOutlet weak var lessonNameTextField: UITextField!
+  @IBOutlet weak var lessonContentTextView: UITextView!
   var unit: Unit!
+  var name: String = ""
+  var content: String = ""
   override func viewDidLoad() {
-    super.viewDidLoad()
+    lessonContentTextView.delegate = self
   }
-
+  
+  @IBAction func LessonNameAdded(_ sender: UITextField) {
+    name = sender.text ?? ""
+  }
+  
+  @IBAction func AddClicked(_ sender: UIBarButtonItem) {
+    
+  }
+  
+  func textViewDidChange(_ textView: UITextView) {
+    content = lessonContentTextView.text ?? ""
+    print("content", content)
+  }
+  
+  func textViewDidBeginEditing(_ textView: UITextView) {
+    if lessonContentTextView.text == "Lesson Content" {
+      textView.text = ""
+      textView.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+  }
+  func textViewDidEndEditing(_ textView: UITextView) {
+    if lessonContentTextView.text == "" {
+      textView.text = "Lesson Content"
+      content = ""
+      textView.textColor = #colorLiteral(red: 0.7490196824, green: 0.7490196824, blue: 0.7490196824, alpha: 1)
+    }
+  }
+  
 }
